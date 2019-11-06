1899c90aae71f62c21c9c3cf61f1865c23b73131
Andi Plummer <andrea@andreaplummer.com>
2019-11-06 15:00:42 -0500

    added add to cart action and thunk to cart.js file in store

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,19 +4,17 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GOT_CART = 'GOT_CART'
-
+const ADD_TO_CART = 'ADD_TO_CART'
+const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 /**
  * INITIAL STATE
  */
-const initialState = {
-  cart: {}
-}
 
 /**
  * ACTION CREATORS
  */
 const gotCart = cart => ({type: GOT_CART, cart})
-
+const addToCart = cart => ({type: ADD_TO_CART, cart})
 /**
  * THUNK CREATORS
  */
@@ -28,15 +26,26 @@ export const getCart = () => async dispatch => {
     console.log(error)
   }
 }
-//a change
+
+export const addItemToCart = breakfastId => async dispatch => {
+  try {
+    const {data} = await axios.post('/api/carts', breakfastId)
+    dispatch(addToCart(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 /**
  * REDUCER
  */
-export default function(state = initialState, action) {
+export default function(cart = {}, action) {
   switch (action.type) {
     case GOT_CART:
       return action.cart
+    case ADD_TO_CART:
+      return action.cart
     default:
-      return state
+      return cart
   }
 }
