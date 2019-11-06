b218c0b589eb57a4dbd27980c186a38e121c6ee5
JWaltuch <jenna.waltuch@gmail.com>
2019-11-05 23:08:10 -0500

    wrote tests for allBreakfasts and breakfast component on front end, also made change to bundle so tests watch

---
+++ client/components/allBreakfasts.spec.js
@@ -0,0 +1,48 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import configureMockStore from 'redux-mock-store'
+import AllBreakfasts from './allBreakfasts'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('All Breakfasts', () => {
+  let breakfasts
+  let breakfastData = [
+    {
+      name: 'Cheerios',
+      pictureUrl:
+        'https://target.scene7.com/is/image/Target/GUEST_cfecc8db-c5ca-46c6-bea1-89fcaad393e2?wid=325&hei=325&qlt=80&fmt=webp',
+      price: 10.0
+    },
+    {
+      name: 'Fruit For Loops',
+      pictureUrl:
+        'https://target.scene7.com/is/image/Target/GUEST_cfecc8db-c5ca-46c6-bea1-89fcaad393e2?wid=325&hei=325&qlt=80&fmt=webp',
+      price: 11.0
+    }
+  ]
+
+  beforeEach(() => {
+    const mockStore = configureMockStore()
+    let store = mockStore({breakfast: {allBreakfasts: breakfastData}})
+    breakfasts = shallow(<AllBreakfasts store={store} />)
+  })
+
+  it('has access to each breakfast item', () => {
+    expect(breakfasts.props().children.props.breakfasts[0].name).to.be.equal(
+      'Cheerios'
+    )
+    expect(breakfasts.props().children.props.breakfasts[1].name).to.be.equal(
+      'Fruit For Loops'
+    )
+  })
+
+  it('renders a list item for each breakfast item in state', () => {
+    expect(breakfasts.render().find('li').length).to.be.equal(2)
+  })
+})
