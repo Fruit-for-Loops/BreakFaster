7169333a1e76c639970a5260f5b282f483aba6c8
Katey Phillips <kateyphi@gmail.com>
2019-11-06 15:42:18 -0500

    models and routes

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-  items: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER)
-  },
   purchased: {
     type: Sequelize.DATE
+  },
+  total: {
+    type: Sequelize.INTEGER
   }
 })
 
---
+++ server/db/models/cartItem.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const CartItem = db.define('cartItem', {
+  quantity: {
+    type: Sequelize.INTEGER
+  },
+  currentPrice: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = CartItem
