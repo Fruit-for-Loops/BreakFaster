7fa0e003f324acf191bb417aa8497e10d2ecf9aa
Andi Plummer <andrea@andreaplummer.com>
2019-11-07 16:41:27 -0500

    tests for cart redux

--- client/components/breakfast.spec.js
+++ client/components/breakfast.spec.js
@@ -5,6 +5,7 @@ import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {Breakfast} from './breakfast'
+// import {spy} from 'sinon'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
@@ -15,7 +16,7 @@ describe('Breakfast', () => {
     name: 'Cheerios',
     pictureUrl:
       'https://target.scene7.com/is/image/Target/GUEST_cfecc8db-c5ca-46c6-bea1-89fcaad393e2?wid=325&hei=325&qlt=80&fmt=webp',
-    price: 10.0
+    price: 1000
   }
 
   beforeEach(() => {
@@ -23,7 +24,12 @@ describe('Breakfast', () => {
   })
 
   it('has access to props of breakfast item', () => {
-    expect(breakfast.find('CardTitle').text()).to.be.equal('Cheerios')
-    expect(breakfast.find('CardSubtitle').text()).to.be.equal('$10')
+    expect(breakfast.contains('Cheerios')).to.equal(true)
+    expect(breakfast.contains(1000)).to.equal(true)
   })
+
+  // it('clicking the add to cart button calls addToCart', () => {
+  //   let addToCartSpy = spy()
+
+  // })
 })
--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,7 +1,13 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {increaseQuantity, decreaseQuantity} from './cart'
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  getCart,
+  removeItemFromCart,
+  addItemToCart
+} from './cart'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -13,6 +19,7 @@ const mockStore = configureMockStore(middlewares)
 describe('thunk creators', () => {
   let store
   let mockAxios
+  let currentCart
 
   const initialState = {
     cart: []
@@ -21,6 +28,20 @@ describe('thunk creators', () => {
   beforeEach(() => {
     mockAxios = new MockAdapter(axios)
     store = mockStore(initialState)
+    currentCart = [
+      {
+        id: 1,
+        name: 'Tofu Scramble Slam',
+        price: 10,
+        stock: 4,
+        cartItem: {
+          quantity: 1,
+          currentPrice: 10,
+          cartId: 1,
+          breakfastId: 1
+        }
+      }
+    ]
   })
 
   afterEach(() => {
@@ -28,21 +49,50 @@ describe('thunk creators', () => {
     store.clearActions()
   })
 
-  describe('increaseQuantity', () => {
+  describe('Cart functions', () => {
+    it('adds a new item to the cart', async () => {
+      mockAxios.onGet('/api/carts').replyOnce(200, currentCart)
+      mockAxios.onPost('/api/carts', 1).replyOnce(201)
+      await store.dispatch(addItemToCart(1))
+      let state = store.getState()
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('ADD_TO_CART')
+      expect(state.cart).to.be.equal(currentCart)
+    })
+
+    it('gets items from a cart', async () => {
+      mockAxios.onGet('/api/carts').replyOnce(200, currentCart)
+      await store.dispatch(getCart())
+      let state = store.getState()
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('GOT_CART')
+      expect(state.cart).to.be.equal(currentCart)
+    })
+
     it('eventually dispatches the UPDATED QUANTITY action', async () => {
       mockAxios.onPut('/api/carts/increase').replyOnce(204)
       await store.dispatch(increaseQuantity(1))
       const actions = store.getActions()
       expect(actions[0].type).to.be.equal('UPDATED_QUANTITY')
     })
-  })
 
-  describe('decreaseQuantity', () => {
     it('eventually dispatches the UPDATED QUANTITY action', async () => {
       mockAxios.onPut('/api/carts/decrease').replyOnce(204)
       await store.dispatch(decreaseQuantity(1))
       const actions = store.getActions()
       expect(actions[0].type).to.be.equal('UPDATED_QUANTITY')
+    })
+
+    it('removes an item from the cart', async () => {
+      mockAxios.onGet('/api/carts').reply(200, currentCart)
+      await store.dispatch(getCart())
+      let state = store.getState()
+      let cartLength = state.cart.length
+      mockAxios.onDelete('/api/carts', 1).replyOnce(204)
+      await store.dispatch(removeItemFromCart(1))
+      state = store.getState()
+      const newCartLength = state.cart.length
+      expect(newCartLength).to.be.equal(cartLength - 1)
     })
   })
 })
