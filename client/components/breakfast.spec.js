f9e8a1b486daa86286da64da70a02b4c836791a3
Andi Plummer <andrea@andreaplummer.com>
2019-11-12 17:12:14 -0500

    all action checks are passing

--- client/components/breakfast.spec.js
+++ client/components/breakfast.spec.js
@@ -4,13 +4,15 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {Breakfast} from './breakfast'
+import Breakfast from './breakfast'
+import configureMockStore from 'redux-mock-store'
+import {Provider} from 'react-redux'
 // import {spy} from 'sinon'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-xdescribe('Breakfast', () => {
+describe('Breakfast', () => {
   let breakfast
   let breakfastData = {
     name: 'Cheerios',
@@ -20,12 +22,27 @@ xdescribe('Breakfast', () => {
   }
 
   beforeEach(() => {
-    breakfast = shallow(<Breakfast breakfast={breakfastData} />)
+    // let mockStore = configureMockStore()
+    // let store = mockStore({})
+    // breakfast = shallow(
+    //   <Provider store={store}>
+    //     <Breakfast breakfast={breakfastData} />
+    //   </Provider>
+    // )
   })
 
-  it('has access to props of breakfast item', () => {
-    expect(breakfast.contains('Cheerios')).to.equal(true)
-    expect(breakfast.contains(1000)).to.equal(true)
+  xit('has access to props of breakfast item', () => {
+    const mockStore = configureMockStore()
+    let store = mockStore({
+      name: 'Cheerios',
+      pictureUrl:
+        'https://target.scene7.com/is/image/Target/GUEST_cfecc8db-c5ca-46c6-bea1-89fcaad393e2?wid=325&hei=325&qlt=80&fmt=webp',
+      price: 1000
+    })
+    let wrapper = shallow(<Breakfast store={store} />)
+    console.log('WRAPPER', wrapper)
+    expect(wrapper.contains('Cherrios')).to.equal(true)
+    expect(wrapper.contains(1000)).to.equal(true)
   })
 
   // it('clicking the add to cart button calls addToCart', () => {
--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -17,7 +17,7 @@ import thunkMiddleware from 'redux-thunk'
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
-xdescribe('thunk creators', () => {
+describe('thunk creators', () => {
   let store
   let mockAxios
   let currentCart
@@ -50,57 +50,47 @@ xdescribe('thunk creators', () => {
     store.clearActions()
   })
 
+  const breakfast = {
+    id: 1,
+    name: 'Tofu Scramble Slam',
+    price: 10,
+    stock: 4
+  }
+
   describe('Cart functions', () => {
     it('adds a new item to the cart', async () => {
-      const breakfast = {
-        id: 1,
-        name: 'Tofu Scramble Slam',
-        price: 10,
-        stock: 4
-      }
-      mockAxios.onGet('/api/carts').replyOnce(200, breakfast)
-      mockAxios.onPost('/api/carts').replyOnce(201)
+      mockAxios.onPost('/api/carts').replyOnce(201, breakfast)
       await store.dispatch(addToCart(breakfast))
-      let state = store.getState()
-      console.log('STATE', state)
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GOT_CART')
-      expect(state.cart).to.be.equal(currentCart)
+      expect(actions[0].type).to.be.equal('ADD_TO_CART')
     })
 
-    xit('gets items from a cart', async () => {
-      mockAxios.onGet('/api/carts').replyOnce(200, currentCart)
+    it('gets items from a cart', async () => {
+      mockAxios.onGet('/api/carts').replyOnce(200)
       await store.dispatch(getCart())
-      let state = store.getState()
-      const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GOT_CART')
-      expect(state.cart).to.be.equal(currentCart)
-    })
-
-    xit('eventually dispatches the UPDATED QUANTITY action', async () => {
-      mockAxios.onPut('/api/carts/increase').replyOnce(204)
-      await store.dispatch(increaseQuantity(1))
       const actions = store.getActions()
       expect(actions[0].type).to.be.equal('GOT_CART')
     })
 
-    xit('eventually dispatches the UPDATED QUANTITY action', async () => {
-      mockAxios.onPut('/api/carts/decrease').replyOnce(204)
-      await store.dispatch(decreaseQuantity(1))
+    it('eventually dispatches the UPDATED QUANTITY action', async () => {
+      mockAxios.onPut('/api/carts/increase').replyOnce(204, breakfast)
+      await store.dispatch(increaseQuantity(breakfast))
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GOT_CART')
+      expect(actions[0].type).to.be.equal('UPDATED_QUANTITY')
     })
 
-    xit('removes an item from the cart', async () => {
-      mockAxios.onGet('/api/carts').reply(200, currentCart)
-      await store.dispatch(getCart())
-      let state = store.getState()
-      let cartLength = state.cart.length
-      mockAxios.onDelete('/api/carts', 1).replyOnce(204)
-      await store.dispatch(removeItemFromCart(1))
-      state = store.getState()
-      const newCartLength = state.cart.length
-      expect(newCartLength).to.be.equal(cartLength - 1)
+    it('eventually dispatches the UPDATED QUANTITY action', async () => {
+      mockAxios.onPut('/api/carts/decrease').replyOnce(204, breakfast)
+      await store.dispatch(decreaseQuantity(breakfast))
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('UPDATED_QUANTITY')
+    })
+
+    it('removes an item from the cart', async () => {
+      mockAxios.onDelete(`/api/carts/${1}`).replyOnce(204, breakfast)
+      await store.dispatch(removeItemFromCart(breakfast))
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('DELETE_FROM_CART')
     })
   })
 })
